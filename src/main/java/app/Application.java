0a165df3978b2d917a7d54c6bbc6781c717360f0
Casper Pijnenburg <casper.pijnenburg@student.fontys.nl>
2017-03-13 15:27:33 +0100

    Travis again

--- src/main/java/app/Application.java
+++ src/main/java/app/Application.java
@@ -6,8 +6,10 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 
 @SpringBootApplication
@@ -17,5 +19,15 @@ public class Application extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(Application.class);
+    }
+
+    @RequestMapping("/")
+    public String home() {
+        return "Welkom!!!";
     }
 }
