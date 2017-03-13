f6b45711a30e8d4394e1ad17b4570582e02b6319
Casper Pijnenburg <casper.pijnenburg@student.fontys.nl>
2017-03-13 15:38:43 +0100

    Chmod gradlew for travis

--- src/main/java/app/Application.java
+++ src/main/java/app/Application.java
@@ -6,10 +6,8 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 
 @SpringBootApplication
@@ -19,15 +17,5 @@ public class Application extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
-    }
-
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-        return builder.sources(Application.class);
-    }
-
-    @RequestMapping("/")
-    public String home() {
-        return "Welkom!!!";
     }
 }
