194bc5a4cf12e648fc6e598f128159cf18ecc909
Reinoud van Zoelen <Reinoud van Zoelen>
2017-03-13 16:25:46 +0100

    Added the User model and the UserBuilder for constructing it

---
+++ src/main/java/model/User.java
@@ -0,0 +1,67 @@
+package model;
+
+import javax.persistence.*;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int _id;
+
+    @Column
+    private String _username;
+
+    @Column
+    private String _password;
+
+    @Column
+    private String _name;
+
+    @Column
+    private boolean _active;
+
+    public User() {
+    }
+
+    User(String username, String password, String name, boolean active) {
+        _username = username;
+        _password = password;
+        _name = name;
+        _active = active;
+    }
+
+    //region Getters & Setters
+    public String get_username() {
+        return _username;
+    }
+
+    public void set_username(String _username) {
+        this._username = _username;
+    }
+
+    public String get_password() {
+        return _password;
+    }
+
+    public void set_password(String _password) {
+        this._password = _password;
+    }
+
+    public String get_name() {
+        return _name;
+    }
+
+    public void set_name(String _name) {
+        this._name = _name;
+    }
+
+    public boolean is_active() {
+        return _active;
+    }
+
+    public void set_active(boolean _active) {
+        this._active = _active;
+    }
+    //endregion
+}
