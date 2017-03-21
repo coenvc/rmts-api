3c5ce7f4e7eac53ce07e317bce79543dea8b74ef
Casper Pijnenburg <casper.pijnenburg@student.fontys.nl>
2017-03-21 14:11:39 +0100

    Refactor

--- src/main/java/model/Prospect.java
+++ src/main/java/model/Prospect.java
@@ -1,5 +1,7 @@
 package model;
 
+import model.status.Status;
+
 import javax.persistence.*;
 
 @Entity
--- src/main/java/model/ProspectBuilder.java
+++ src/main/java/model/ProspectBuilder.java
@@ -1,5 +1,7 @@
 package model;
 
+import model.status.Status;
+
 public class ProspectBuilder {
 
     private int id;
