6ca95f07e9c10498fbb051ad143c663ede3da58b
Niels Werkman <Nielswerkman93@gmail.com>
2017-04-10 15:02:41 +0200

    Action model + Builder added

---
+++ src/main/java/model/Action.java
@@ -0,0 +1,92 @@
+package model;
+
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+public class Action {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+
+    @Column
+    private ActionType actionType;
+
+    @Column
+    private Date date;
+
+    @Column
+    private String description;
+
+    @Column
+    private User user;
+
+    @Column
+    private boolean isCompleted;
+
+    public Action(){
+
+    }
+
+    public Action(ActionType actionType, Date date, String description, User user, boolean isCompleted) {
+        this.actionType = actionType;
+        this.date = date;
+        this.description = description;
+        this.user = user;
+        this.isCompleted = isCompleted;
+    }
+
+    //region Setters & Getters
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public ActionType getActionType() {
+        return actionType;
+    }
+
+    public void setActionType(ActionType actionType) {
+        this.actionType = actionType;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public boolean isCompleted() {
+        return isCompleted;
+    }
+
+    public void setCompleted(boolean completed) {
+        isCompleted = completed;
+    }
+
+    //endregion
+}
---
+++ src/main/java/model/ActionBuilder.java
@@ -0,0 +1,58 @@
+package model;
+
+import java.util.Date;
+
+public class ActionBuilder {
+
+    private int id;
+    private ActionType actionType;
+    private Date date;
+    private String description;
+    private User user;
+    private boolean isCompleted = false;
+
+    public ActionBuilder id(int id){
+        this.id = id;
+        return this;
+    }
+
+    public ActionBuilder actionType(ActionType actionType){
+        this.actionType = actionType;
+        return this;
+    }
+
+    public ActionBuilder date(Date date){
+        this.date = date;
+        return this;
+    }
+
+    public ActionBuilder description(String description){
+        this.description = description;
+        return this;
+    }
+
+    public ActionBuilder user(User user){
+        this.user = user;
+        return this;
+    }
+
+    public ActionBuilder isComplete(boolean isCompleted){
+        this.isCompleted = isCompleted;
+        return this;
+    }
+
+    public Action build(){
+        checkProperties();
+        return new Action(actionType, date, description, user, isCompleted);
+    }
+
+    //region Helper methods
+    private void checkProperties() {
+        if (actionType == null) throw new IllegalArgumentException("No actionType specified.");
+        if (date == null) throw new IllegalArgumentException("No date specified.");
+        if (description == null) throw new IllegalArgumentException("No description specified.");
+        if (user == null) throw new IllegalArgumentException("No user specified.");
+    }
+    //endregion
+
+}
