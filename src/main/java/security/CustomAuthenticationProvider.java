49e1b25687ec1c7a20f9c5633f9b2c8b63bb789a
Casper Pijnenburg <casper.pijnenburg@student.fontys.nl>
2017-05-22 10:38:47 +0200

    Security v2

--- src/main/java/security/CustomAuthenticationProvider.java
+++ src/main/java/security/CustomAuthenticationProvider.java
@@ -1,40 +1,40 @@
- package security;
+package security;
 
- import model.user.User;
- import org.springframework.beans.factory.annotation.Autowired;
- import org.springframework.context.annotation.ComponentScan;
- import org.springframework.security.authentication.*;
- import org.springframework.security.core.Authentication;
- import org.springframework.security.core.AuthenticationException;
- import org.springframework.security.core.GrantedAuthority;
- import org.springframework.security.core.authority.SimpleGrantedAuthority;
- import org.springframework.stereotype.Component;
- import repository.user.HibernateUserRepository;
+import model.user.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.security.authentication.*;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.stereotype.Component;
+import repository.user.HibernateUserRepository;
 
- import java.util.ArrayList;
- import java.util.List;
-
-/**
- * Created by BonoIJpelaar on 24-04-17.
- */
+import java.util.ArrayList;
+import java.util.List;
 
 @Component
 @ComponentScan("repository.user")
 public class CustomAuthenticationProvider implements AuthenticationProvider {
 
+    private final HibernateUserRepository userService;
+
     @Autowired
-    HibernateUserRepository userService;
+    public CustomAuthenticationProvider(HibernateUserRepository userService) {
+        this.userService = userService;
+    }
 
     public Authentication authenticate(Authentication authentication) throws AuthenticationException {
         String username = authentication.getPrincipal().toString();
         String password = authentication.getCredentials().toString();
 
         User user = userService.login(username, password);
-        if(user == null) {
+        if (user == null) {
             throw new BadCredentialsException("Invalid username/password");
         }
 
-        if(!user.isActive()) {
+        if (!user.isActive()) {
             throw new DisabledException("User is inactive");
         }
 
--- src/main/java/security/WebSecurityConfig.java
+++ src/main/java/security/WebSecurityConfig.java
@@ -6,30 +6,36 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    private final CustomAuthenticationProvider authProvider;
 
     @Autowired
-    private CustomAuthenticationProvider authProvider;
+    public WebSecurityConfig(CustomAuthenticationProvider authProvider) {
+        this.authProvider = authProvider;
+    }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth
-                .authenticationProvider(authProvider);
+        auth.authenticationProvider(authProvider);
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        String host = "http://localhost:4200/";
         http
                 .authorizeRequests()
-                .anyRequest().permitAll()
-                .and()
+                    .anyRequest().authenticated()
+                    .and()
                 .formLogin()
-                .and()
+                    .loginPage(host)
+                    .permitAll()
+                    .and()
                 .httpBasic()
-                .and()
+                    .and()
                 .csrf().disable();
     }
 }
